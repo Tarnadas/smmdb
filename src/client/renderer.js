c18fd45c5796f0185e40eaf30bee70948015f1a8
Mario <m_reder10@cs.uni-kl.de>
2017-06-12 17:38:08 +0200

    removed old code

--- src/client/renderer.js
+++ src/client/renderer.js
@@ -10,7 +10,7 @@ import Socket      from './socket'
 import initReducer from './reducers'
 import AppView     from './components/views/AppView'
 
-export const domain = 'http://tarnadas.ddns.net';
+export const domain = 'http://tarnadas.ddns.net:3000';
 
 const socket = new Socket();
 const store = initReducer(socket);
