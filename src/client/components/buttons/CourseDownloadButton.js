68bca846baf5fab55101983fc1ca7b4eb9c505f8
Mario <m_reder10@cs.uni-kl.de>
2017-06-21 17:31:58 +0200

    copy course ID on 3DS

--- src/client/components/buttons/CourseDownloadButton.js
+++ src/client/components/buttons/CourseDownloadButton.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import MediaQuery from 'react-responsive'
+import copy from 'copy-to-clipboard'
 
 export default class CourseDownloadButton extends React.PureComponent {
+    constructor (props) {
+        super(props);
+        this.onCopyCourseID = this.onCopyCourseID.bind(this, props.courseId);
+    }
+    onCopyCourseID (courseID) {
+        copy(courseID);
+    }
     render () {
         const styles = {
             href: {
@@ -9,6 +18,7 @@ export default class CourseDownloadButton extends React.PureComponent {
                 margin: '0 5px',
             },
             button: {
+                height: '180px',
                 backgroundColor: '#11c2b0',
                 color: '#fff',
                 borderRadius: '5px',
@@ -31,16 +41,33 @@ export default class CourseDownloadButton extends React.PureComponent {
             }
         };
         return (
-            <a style={styles.href} href={`/api/downloadcourse?id=${this.props.courseId}&type=zip`} download>
-                <div style={styles.button}>
-                    <div style={styles.icon}>
-                        <img style={styles.iconImg} src="/img/coursebot.png" />
-                    </div>
-                    <div style={styles.text}>
-                        Download
-                    </div>
-                </div>
-            </a>
+            <MediaQuery query='screen and (min-device-width: 320px) and (max-device-width: 320px) and (min-device-height: 240px) and (max-device-width: 240px)'>
+                {
+                    matches => (
+                        matches ? (
+                            <div style={styles.button} onClick={this.onCopyCourseID}>
+                                <div style={styles.icon}>
+                                    <img style={styles.iconImg} src="/img/coursebot.png" />
+                                </div>
+                                <div style={styles.text}>
+                                    Copy Course ID
+                                </div>
+                            </div>
+                        ) : (
+                            <a style={styles.href} href={`/api/downloadcourse?id=${this.props.courseId}&type=zip`} download>
+                                <div style={styles.button}>
+                                    <div style={styles.icon}>
+                                        <img style={styles.iconImg} src="/img/coursebot.png" />
+                                    </div>
+                                    <div style={styles.text}>
+                                        Download
+                                    </div>
+                                </div>
+                            </a>
+                        )
+                    )
+                }
+            </MediaQuery>
         )
     }
 }
