3e38873eb160146dd60dc863b6408b6175e24cd0
Mario <m_reder10@cs.uni-kl.de>
2017-08-08 15:12:56 +0200

    grammar

--- src/client/components/views/MainView.js
+++ src/client/components/views/MainView.js
@@ -36,7 +36,7 @@ class MainView extends React.PureComponent {
           This is a website solely for the purpose to share Super Mario Maker courses platform independently.
           It doesn't matter if you want to use courses on this website for Wii U, 3DS or an emulator. All platforms are supported.<br /><br />
           To use all features on this website it is recommended to sign in with Google.<br /><br />
-          All shared content on this website is user-created. We do not share any copyrighted stuff.
+          All content on this website is user-created. We do not share any copyrighted stuff.
         </div>
       </div>
     )
