fa8f35ad9357791a449ad68e3d4d69b41e78ab47
Mario Reder <m_reder10@cs.uni-kl.de>
2018-04-18 08:41:26 +0200

    chore(build): Moved Webpack configs to separate folder

---
+++ config/environment.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dev: {
+    port: 4200,
+    domain: 'localhost'
+  },
+  ver: {
+    port: 3001,
+    domain: 'tarnadas.ddns.net'
+  },
+  prod: {
+    port: 3000,
+    domain: 'smmdb.ddns.net'
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 module.exports = function(env) {
-  return require(`./webpack.${env}.js`)
+  return require(`./config/webpack.${env}.js`)
 }
