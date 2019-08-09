efdeeb31c41f9ba66d902bc9e96123f04a4fa8fb
Mario Reder <mreder1289@gmail.com>
2019-08-09 16:52:33 +0200

    ci: test env variable passing

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker-compose build
+echo $GOOGLE_CLIENT_ID
+echo $DISCORD_TOKEN
+$GOOGLE_CLIENT_ID $DISCORD_TOKEN docker-compose build
 docker-compose bundle --push-images
