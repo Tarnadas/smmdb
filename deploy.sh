979252ffe31f2812ffe5201e83a3dd8f2ca0a5a7
Mario Reder <mreder1289@gmail.com>
2019-08-10 21:37:24 +0200

    ci: pass env variable

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo $GOOGLE_CLIENT_ID
-echo $DISCORD_TOKEN
-$GOOGLE_CLIENT_ID $DISCORD_TOKEN docker-compose build
+GOOGLE_CLIENT_ID=$GOOGLE_CLIENT_ID DISCORD_TOKEN=$DISCORD_TOKEN docker-compose build
 docker-compose bundle --push-images
