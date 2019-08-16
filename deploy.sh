077f34497f0b931d3744ad6dc20386760719bdfc
Mario Reder <mreder1289@gmail.com>
2019-08-16 21:37:34 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker-compose pull
-GOOGLE_CLIENT_ID="$GOOGLE_CLIENT_ID" DISCORD_TOKEN="$DISCORD_TOKEN" docker-compose build --parallel
+docker-compose build --build-arg GOOGLE_CLIENT_ID --build-arg DISCORD_TOKEN --parallel
 docker-compose bundle --push-images
