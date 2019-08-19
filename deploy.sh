7c3499523dd4df5a9c76d73dfd666f9130780208
Mario Reder <mreder1289@gmail.com>
2019-08-19 23:16:52 +0200

    remove static docker images

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker-compose -f docker-compose.static.yml pull
-docker-compose -f docker-compose.static.yml build --parallel
-docker-compose -f docker-compose.static.yml bundle --push-images
-
 docker-compose -f docker-compose.build.yml pull
 docker-compose -f docker-compose.build.yml build --parallel
 docker-compose -f docker-compose.build.yml bundle --push-images
