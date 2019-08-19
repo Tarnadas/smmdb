0bd9f3f2be2cf5bdebdaa527c0f7c22f9cf90c69
Mario Reder <mreder1289@gmail.com>
2019-08-19 09:04:49 +0200

    split docker build into multiple files

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+docker-compose -f docker-compose.build.yml pull
+docker-compose -f docker-compose.build.yml build --parallel
+docker-compose -f docker-compose.build.yml bundle --push-images
+
 docker-compose pull
 docker-compose build --parallel
 docker-compose bundle --push-images
