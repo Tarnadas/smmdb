ed4b40a7f4adca540aeaa260cafd1a48e0e4aefc
Mario Reder <mreder1289@gmail.com>
2019-08-27 17:27:36 +0200

    courses2 put request

--- api/src/routes/courses2/put.rs
+++ api/src/routes/courses2/put.rs
@@ -1,36 +1,67 @@
 use crate::server::ServerData;
 use crate::Database;
 
-use actix_web::{error, http::StatusCode, put, web, Error, HttpRequest, HttpResponse};
-use futures::{future::result, Future, Stream};
+use cemu_smm::{course2::Course2, errors::DecompressionError, proto::SMM2Course::SMM2Course};
+
+use actix_web::{
+    error::{PayloadError, ResponseError},
+    http::StatusCode,
+    put,
+    web::{self, BytesMut},
+    HttpRequest, HttpResponse,
+};
+use futures::{self, Future, Stream};
 
 #[put("")]
 pub fn put_courses(
     data: web::Data<ServerData>,
     req: HttpRequest,
     payload: web::Payload,
-) -> impl Future<Item = HttpResponse, Error = Error> {
+) -> impl Future<Item = HttpResponse, Error = PutCourses2Error> {
     // data.get_courses2(query.into_inner())
     payload
         .from_err()
-        .fold((), |_, chunk| {
-            println!("Chunk: {:?}", chunk);
-            result::<_, error::PayloadError>(Ok(()))
+        .fold(BytesMut::new(), |mut acc, chunk| {
+            acc.extend_from_slice(&chunk);
+            Ok::<BytesMut, PayloadError>(acc)
         })
-        .map(|a| {
-            dbg!(a);
-            HttpResponse::Ok().into()
+        .map(|buffer| match Course2::from_packed(&buffer[..]) {
+            Ok(courses) => {
+                let courses: Vec<SMM2Course> = courses
+                    .into_iter()
+                    .map(|course| course.take_course())
+                    .collect();
+                HttpResponse::Ok().json(courses)
+            }
+            Err(err) => PutCourses2Error::from(err).error_response(),
         })
-    // Ok("".to_owned())
 }
 
-// #[derive(Debug, Display)]
-// enum PutCourses2Error {}
+#[derive(Fail, Debug)]
+pub enum PutCourses2Error {
+    #[fail(display = "{}", _0)]
+    PayloadError(PayloadError),
+    #[fail(display = "placeholder")]
+    DecompressionError(DecompressionError),
+}
 
-// impl ResponseError for PutCourses2Error {
-//     fn error_response(&self) -> HttpResponse {
-//         match *self {
-//             _ => HttpResponse::new(StatusCode::BAD_REQUEST),
-//         }
-//     }
-// }
+impl From<PayloadError> for PutCourses2Error {
+    fn from(err: PayloadError) -> Self {
+        PutCourses2Error::PayloadError(err)
+    }
+}
+
+impl From<DecompressionError> for PutCourses2Error {
+    fn from(err: DecompressionError) -> Self {
+        PutCourses2Error::DecompressionError(err)
+    }
+}
+
+impl ResponseError for PutCourses2Error {
+    fn error_response(&self) -> HttpResponse {
+        match *self {
+            PutCourses2Error::PayloadError(_) => HttpResponse::new(StatusCode::BAD_REQUEST),
+            PutCourses2Error::DecompressionError(_) => HttpResponse::new(StatusCode::BAD_REQUEST),
+        }
+    }
+}
