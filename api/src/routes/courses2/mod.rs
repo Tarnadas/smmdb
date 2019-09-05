e0e4d5f5af762dfbe3fa9f98d946b8b6b76f4477
Mario Reder <mreder1289@gmail.com>
2019-09-05 15:57:05 +0200

    add request to analyze smm2 courses

--- api/src/routes/courses2/mod.rs
+++ api/src/routes/courses2/mod.rs
@@ -1,12 +1,15 @@
 mod get;
+mod post;
 mod put;
 
 use actix_web::{dev, web};
 pub use get::*;
+pub use post::*;
 pub use put::*;
 
 pub fn service() -> impl dev::HttpServiceFactory {
     web::scope("/courses2")
         .service(get::get_courses)
         .service(put::put_courses)
+        .service(post::post_analyze_courses)
 }
--- api/src/routes/courses2/post.rs
+++ api/src/routes/courses2/post.rs
@@ -5,18 +5,18 @@ use cemu_smm::{course2::Course2, errors::DecompressionError, proto::SMM2Course::
 use actix_web::{
     error::{PayloadError, ResponseError},
     http::StatusCode,
-    put,
+    post,
     web::{self, BytesMut},
     HttpRequest, HttpResponse,
 };
 use futures::{self, Future, Stream};
 
-#[put("")]
-pub fn put_courses(
+#[post("analyze")]
+pub fn post_analyze_courses(
     data: web::Data<ServerData>,
     req: HttpRequest,
     payload: web::Payload,
-) -> impl Future<Item = HttpResponse, Error = PutCourses2Error> {
+) -> impl Future<Item = HttpResponse, Error = PostCourses2Error> {
     payload
         .from_err()
         .fold(BytesMut::new(), |mut acc, chunk| {
@@ -31,35 +31,35 @@ pub fn put_courses(
                     .collect();
                 HttpResponse::Ok().json(courses)
             }
-            Err(err) => PutCourses2Error::from(err).error_response(),
+            Err(err) => PostCourses2Error::from(err).error_response(),
         })
 }
 
 #[derive(Fail, Debug)]
-pub enum PutCourses2Error {
-    #[fail(display = "PutCourses2Error::Payload: {}", _0)]
+pub enum PostCourses2Error {
+    #[fail(display = "PostCourses2Error::Payload: {}", _0)]
     Payload(PayloadError),
-    #[fail(display = "PutCourses2Error::Decompression: {}", _0)]
+    #[fail(display = "PostCourses2Error::Decompression: {}", _0)]
     Decompression(DecompressionError),
 }
 
-impl From<PayloadError> for PutCourses2Error {
+impl From<PayloadError> for PostCourses2Error {
     fn from(err: PayloadError) -> Self {
-        PutCourses2Error::Payload(err)
+        PostCourses2Error::Payload(err)
     }
 }
 
-impl From<DecompressionError> for PutCourses2Error {
+impl From<DecompressionError> for PostCourses2Error {
     fn from(err: DecompressionError) -> Self {
-        PutCourses2Error::Decompression(err)
+        PostCourses2Error::Decompression(err)
     }
 }
 
-impl ResponseError for PutCourses2Error {
+impl ResponseError for PostCourses2Error {
     fn error_response(&self) -> HttpResponse {
         match *self {
-            PutCourses2Error::Payload(_) => HttpResponse::new(StatusCode::BAD_REQUEST),
-            PutCourses2Error::Decompression(_) => HttpResponse::new(StatusCode::BAD_REQUEST),
+            PostCourses2Error::Payload(_) => HttpResponse::new(StatusCode::BAD_REQUEST),
+            PostCourses2Error::Decompression(_) => HttpResponse::new(StatusCode::BAD_REQUEST),
         }
     }
 }
