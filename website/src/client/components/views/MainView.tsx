dde5c2ccb5f366c5d4946adbec6d2e80f4ce1808
Mario Reder <mreder1289@gmail.com>
2019-11-04 17:44:49 +0100

    update description and affiliates

--- website/src/client/components/views/MainView.tsx
+++ website/src/client/components/views/MainView.tsx
@@ -44,38 +44,71 @@ class MainView extends React.PureComponent<any, any> {
       }
     }
     return (
-      <div style={styles.main} id='scroll'>
+      <div style={styles.main} id="scroll">
         <div style={styles.content}>
-          <div style={styles.header}>
-            Welcome to SMMDB!
-          </div>
-          You can share your Super Mario Maker courses platform independently on this website. Supported platforms are Wii U, 3DS and Cemu. For Cemu there is even a save file editor. Just navigate to &#39;Client&#39;.<br />
-          It also features courses for Super Mario 64 Maker, a popular ROM hack by Kaze Emanuar.<br /><br />
-          To use all features on this website it is recommended to sign in with Google.<br /><br />
-          All content on this website is user-created. We do not share any copyrighted stuff.
+          <div style={styles.header}>Welcome to SMMDB!</div>
+          SMMDB is the only cross console/emulator sharing platform for Super
+          Mario Maker and Super Mario Maker 2 courses.
+          <br />
+          <br />
+          Super Mario Maker 2 courses can finally be uploaded as well!
+          <br />
+          <br />
+          Supported platforms for Super Mario Maker 1 are Wii U, 3DS and Cemu.
+          SMMDB can be used as a compatibility layer between those platform. It
+          enriches .. especially for platforms with minor support from Nintendo,
+          which includes 3DS and emulators.
+          <br />
+          Super Mario Maker for 3DS has no option to search for courses. Several
+          Wii U courses can&#39;t even be played on 3DS because of
+          incompatibility. SMMDB will let you search for courses and tries to
+          convert incompatible courses as good as possible. For Cemu, a Wii U
+          emulator, there is even a save file editor. Just navigate to
+          &#39;Client&#39;.
+          <br />
+          <br />
+          SMMDB also features courses for Super Mario 64 Maker, a popular ROM
+          hack by Kaze Emanuar.
+          <br />
+          <br />
+          To use all features on this website it is recommended to sign in with
+          Google.
+          <br />
+          <br />
+          All content on this website is user-created. We do not share any
+          copyrighted stuff.
         </div>
         <div style={styles.affiliate}>
-          <div style={styles.header}>
-            Affiliates
-          </div>
+          <div style={styles.header}>Affiliates</div>
           <SMMButton
-            link='https://net64-mod.github.io' blank
-            text='Net64 Website'
-            iconSrc='/img/net64.svg'
-            iconColor='bright'
+            link="https://net64-mod.github.io"
+            blank
+            text="Net64 Website"
+            iconSrc="/img/net64.svg"
+            iconColor="bright"
           />
           <SMMButton
-            link='http://mariomods.net/' blank
-            text='Mario Making Mods'
-            iconSrc='/img/MMM.png'
-            iconColor='bright'
+            link="http://mariomods.net/"
+            blank
+            text="Mario Making Mods"
+            iconSrc="/img/MMM.png"
+            iconColor="bright"
             noText
           />
           <SMMButton
-            link='http://sm64hacks.com/' blank
-            text='Super Mario 64 Hacks'
-            iconSrc='/img/sm64hacks.png'
-            iconColor='bright'
+            link="http://sm64hacks.com/"
+            blank
+            text="Super Mario 64 Hacks"
+            iconSrc="/img/sm64hacks.png"
+            iconColor="bright"
+            noText
+          />
+          <SMMButton
+            link="https://smmserver.github.io/"
+            blank
+            text="SmmServer"
+            iconSrc="/img/smmserver.png"
+            iconColor="bright"
             noText
           />
         </div>
@@ -83,6 +116,8 @@ class MainView extends React.PureComponent<any, any> {
     )
   }
 }
-export default connect((state: any): any => ({
-  screenSize: state.getIn(['mediaQuery', 'screenSize'])
-}))(MainView)
+export default connect(
+  (state: any): any => ({
+    screenSize: state.getIn(['mediaQuery', 'screenSize'])
+  })
+)(MainView)
