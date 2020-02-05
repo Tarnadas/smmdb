fde6653a47dc92d26fb018b921d0ca3ff87daea0
Mario Reder <mreder1289@gmail.com>
2020-02-05 20:43:21 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,10 @@
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker-compose -f docker-compose.build.yml pull
+docker-compose -f docker-compose.build.yml pull --ignore-pull-failures
 docker-compose -f docker-compose.build.yml build --parallel
-docker-compose -f docker-compose.build.yml bundle
-docker-compose -f docker-compose.build.yml push
+docker-compose -f docker-compose.build.yml bundle --push-images
 
-docker-compose pull
+docker-compose pull --ignore-pull-failures
 docker-compose build --parallel
-docker-compose bundle
-docker-compose push
+docker-compose bundle --push-images
