90024d12fc75e2ebaccf811984c41be716fb019f
Mario Reder <mreder1289@gmail.com>
2020-02-05 16:19:11 +0100

    try other deploy method

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker-compose -f docker-compose.build.yml pull
 docker-compose -f docker-compose.build.yml build --parallel
-docker-compose -f docker-compose.build.yml bundle --push-images
+docker-compose -f docker-compose.build.yml bundle
+docker-compose -f docker-compose.build.yml push
 
 docker-compose pull
 docker-compose build --parallel
-docker-compose bundle --push-images
+docker-compose bundle
+docker-compose push
