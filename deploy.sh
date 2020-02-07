44e7a2078961ba5b9857ce68b2b2fd234e536829
Mario Reder <mreder1289@gmail.com>
2020-02-07 14:45:07 +0100

    manually tag images

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,13 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker-compose -f docker-compose.build.yml pull --ignore-pull-failures
 docker-compose -f docker-compose.build.yml build --parallel
+docker tag tarnadas/smmdb-build:latest
+docker tag tarnadas/smmdb-build-dep:latest
+docker tag tarnadas/smmdb-api-build:latest
 docker-compose -f docker-compose.build.yml bundle --push-images
 
 docker-compose pull --ignore-pull-failures
 docker-compose build --parallel
+docker tag tarnadas/smmdb:latest
+docker tag tarnadas/smmdb-api:latest
 docker-compose bundle --push-images
