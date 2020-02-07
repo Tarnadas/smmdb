3621ae1134964165f966566397eae370102f9a8c
Mario Reder <mreder1289@gmail.com>
2020-02-07 15:06:05 +0100

    fix docker tag command

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker-compose -f docker-compose.build.yml pull --ignore-pull-failures
 docker-compose -f docker-compose.build.yml build --parallel
-docker tag tarnadas/smmdb-build:$GIT_HASH
-docker tag tarnadas/smmdb-build-dep:$GIT_HASH
-docker tag tarnadas/smmdb-api-build:$GIT_HASH
+docker tag tarnadas/smmdb-build tarnadas/smmdb-build:$GIT_HASH
+docker tag tarnadas/smmdb-build-dep tarnadas/smmdb-build-dep:$GIT_HASH
+docker tag tarnadas/smmdb-api-build tarnadas/smmdb-api-build:$GIT_HASH
 docker-compose -f docker-compose.build.yml bundle --push-images
 
 docker-compose pull --ignore-pull-failures
 docker-compose build --parallel
-docker tag tarnadas/smmdb:$GIT_HASH
-docker tag tarnadas/smmdb-api:$GIT_HASH
+docker tag tarnadas/smmdb tarnadas/smmdb:$GIT_HASH
+docker tag tarnadas/smmdb-api tarnadas/smmdb-api:$GIT_HASH
 docker-compose bundle --push-images
