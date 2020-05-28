ecca73daab3dd464161fd01b86757c4f30275fd6
Mario Reder <mreder1289@gmail.com>
2020-05-28 08:26:06 +0200

    fix and optimize deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,30 +4,26 @@ GIT_HASH=$(git rev-parse HEAD)
 echo "using git hash $GIT_HASH"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker pull tarnadas/smmdb-build &
-docker pull tarnadas/smmdb-build-dep &
-wait
-
-(cd website && docker build --cache-from=tarnadas/smmdb-build -f ./DockerfileBuild .) &
-(cd website && docker build --cache-from=tarnadas/smmdb-build-dep -f ./DockerfileBuildDep .) &
-wait -n
-
 (
+  docker pull tarnadas/smmdb-build
+  docker build --cache-from=tarnadas/smmdb-build -f ./DockerfileBuild .
   docker push tarnadas/smmdb-build:latest
   docker tag tarnadas/smmdb-build tarnadas/smmdb-build:$GIT_HASH
   docker push tarnadas/smmdb-build:$GIT_HASH
 ) &
 
 (
+  docker pull tarnadas/smmdb-build-dep
+  docker build --cache-from=tarnadas/smmdb-build-dep -f ./DockerfileBuildDep .
   docker push tarnadas/smmdb-build-dep:latest
   docker tag tarnadas/smmdb-build-dep tarnadas/smmdb-build-dep:$GIT_HASH
   docker push tarnadas/smmdb-build-dep:$GIT_HASH
 ) &
 
-wait -n
+wait
 
-docker-compose pull --ignore-pull-failures
-docker-compose build --parallel
+docker pull tarnadas/smmdb
+docker build --cache-from=tarnadas/smmdb .
 docker tag tarnadas/smmdb tarnadas/smmdb:$GIT_HASH
 docker push tarnadas/smmdb:latest
 docker push tarnadas/smmdb:$GIT_HASH
